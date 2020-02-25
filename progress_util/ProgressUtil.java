89e6f81b64f5436635f9b64e463055d4c597d37a
sagar <snkumar.nayak@gmail.com>
2020-02-25 19:13:49 +0530

    added upload progress methods

--- progress_util/ProgressUtil.java
+++ progress_util/ProgressUtil.java
@@ -6,6 +6,11 @@ import android.graphics.drawable.ColorDrawable
 
 
 class ProgressUtil(private val context: Context) {
+
+    companion object {
+        const val UPLOAD_PROGRESS = "uploadProgress"
+    }
+	
     private lateinit var dialog: Dialog
     private var count = 0
 
@@ -46,5 +51,51 @@ class ProgressUtil(private val context: Context) {
             dialog.dismiss()
 
         count = 0
+    }
+
+    private lateinit var broadcastAction: String
+
+    fun showProgress(broadcastAction: String = ""): String {
+        if (broadcastAction == "") {
+            this.broadcastAction = UUID.randomUUID().toString()
+        } else {
+            this.broadcastAction = broadcastAction
+        }
+
+        registerForBroadcast()
+        return this.broadcastAction
+    }
+
+    private fun registerForBroadcast() {
+        context.registerReceiver(
+            object : BroadcastReceiver() {
+                override fun onReceive(context: Context?, intent: Intent?) {
+                    intent?.let {
+                        setProgress(
+                            it.getIntExtra(
+                                UPLOAD_PROGRESS,
+                                0
+                            )
+                        )
+                    }
+                }
+            },
+            IntentFilter(
+                this.broadcastAction
+            )
+        )
+    }
+
+    fun setProgress(progress: Int) {
+        val progressBar = dialog.findViewById<ProgressBar>(R.id.progressBar)
+        if (progress == 0 || progress == 100) {
+            progressBar.isIndeterminate = true
+        } else {
+            progressBar.isIndeterminate = false
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
+                progressBar.setProgress(progress, true)
+            else
+                progressBar.progress = progress
+        }
     }
 }
