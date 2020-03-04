24e7ef1a3893ca4330764e590c5ddf034c2adb18
sagar <snkumar.nayak@gmail.com>
2020-03-04 19:18:23 +0530

    added custom imageview widget and image zoom view util files

---
+++ widget/image_view/ImageView.kt
@@ -0,0 +1,423 @@
+package <YOUR PACKAGE HERE>;
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.drawable.Drawable
+import android.os.Handler
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.ImageView
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.sagar.android.pinchandzoom.databinding.CustomImageViewBinding
+import com.squareup.picasso.Callback
+import com.squareup.picasso.Picasso
+import com.squareup.picasso.RequestCreator
+import java.io.File
+import java.util.*
+
+
+/*
+the attrs req are -
+
+    <declare-styleable name="ImageView">
+        <attr name="scaleType" format="enum">
+            <enum name="matrix" value="0" />
+            <enum name="fitXY" value="1" />
+            <enum name="fitStart" value="2" />
+            <enum name="fitCenter" value="3" />
+            <enum name="fitEnd" value="4" />
+            <enum name="center" value="5" />
+            <enum name="centerCrop" value="6" />
+            <enum name="centerInside" value="7" />
+        </attr>
+        <attr name="mode" format="enum">
+            <enum name="download" value="0" />
+            <enum name="upload" value="1" />
+        </attr>
+        <attr name="enableZoom" format="enum">
+            <enum name="yes" value="1" />
+            <enum name="no" value="0" />
+        </attr>
+    </declare-styleable>
+	
+dependency req -
+    implementation 'com.mikhaellopez:circularprogressbar:3.0.3'
+*/
+@Suppress("unused", "MemberVisibilityCanBePrivate")
+class ImageView @JvmOverloads constructor(
+    context: Context? = null,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) :
+    ConstraintLayout(context, attrs, defStyleAttr) {
+
+    enum class Mode {
+        DOWNLOAD, UPLOAD
+    }
+
+    enum class ZoomEnabled {
+        YES, NO
+    }
+
+    enum class ImageLoadedVia {
+        BITMAP, DRAWABLE, URL
+    }
+
+    companion object {
+        const val UPLOAD_UPDATE = "uploadUpdate"
+    }
+
+    lateinit var contextForLaterUse: Context
+    lateinit var binding: CustomImageViewBinding
+    private var mode: Mode = Mode.DOWNLOAD
+    var scaleType: ImageView.ScaleType = ImageView.ScaleType.CENTER_CROP
+    private var identifier: String = ""
+
+    lateinit var imageLoadedVia: ImageLoadedVia
+    lateinit var imageUrl: String
+    lateinit var imageBitmap: Bitmap
+    lateinit var imageDrawable: Drawable
+
+    private val imageViewDoubleClickListener: OnClickListener = DoubleClickListener(
+        callback = object : DoubleClickListener.Callback {
+            override fun doubleClicked() {
+                changeZoomOfImage()
+            }
+        }
+    )
+
+    init {
+        context?.let {
+            binding = CustomImageViewBinding.inflate(
+                it.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater,
+                this,
+                true
+            )
+            contextForLaterUse = it
+        }
+
+        attrs?.let {
+            val typedArray = context?.obtainStyledAttributes(
+                it, R.styleable.ImageView
+            )
+            typedArray?.let {
+                val type = typedArray.getInteger(
+                    R.styleable.ImageView_scaleType, 6
+                )
+                setScaleType(type)
+                val mode = typedArray.getInteger(
+                    R.styleable.ImageView_mode, 0
+                )
+                when (mode) {
+                    0 -> setMode(Mode.DOWNLOAD)
+                    1 -> setMode(Mode.UPLOAD)
+                }
+                val isZoomEnabled = typedArray.getInteger(
+                    R.styleable.ImageView_enableZoom, 0
+                )
+                when (isZoomEnabled) {
+                    0 -> configureZoom(ZoomEnabled.NO)
+                    1 -> configureZoom(ZoomEnabled.YES)
+                }
+                initiate()
+            } ?: run {
+                initiate()
+            }
+            typedArray?.recycle()
+        }
+    }
+
+    fun setScaleType(type: Int? = null, scaleType: ImageView.ScaleType? = null) {
+        type?.let {
+            binding.imageView.scaleType = when (it) {
+                0 -> ImageView.ScaleType.MATRIX
+                1 -> ImageView.ScaleType.FIT_XY
+                2 -> ImageView.ScaleType.FIT_START
+                3 -> ImageView.ScaleType.FIT_CENTER
+                4 -> ImageView.ScaleType.FIT_END
+                5 -> ImageView.ScaleType.CENTER
+                6 -> ImageView.ScaleType.CENTER_CROP
+                7 -> ImageView.ScaleType.CENTER_INSIDE
+                else -> ImageView.ScaleType.CENTER_CROP
+            }
+        }
+
+        scaleType?.let {
+            binding.imageView.scaleType = it
+        }
+
+        this.scaleType = binding.imageView.scaleType
+    }
+
+    private fun setMode(mode: Mode) {
+        this.mode = mode
+
+        initiate()
+    }
+
+    private fun configureZoom(zoomEnabled: ZoomEnabled) {
+        if (zoomEnabled == ZoomEnabled.YES) {
+            binding.imageView.setOnClickListener(imageViewDoubleClickListener)
+        } else {
+            binding.imageView.setOnClickListener(null)
+        }
+    }
+
+    private fun initiate() {
+        when (mode) {
+            Mode.DOWNLOAD -> {
+                binding.progressDownload.visibility = View.VISIBLE
+                binding.progressUpload.visibility = View.GONE
+            }
+            Mode.UPLOAD -> {
+                binding.progressDownload.visibility = View.GONE
+                binding.progressUpload.visibility = View.VISIBLE
+            }
+        }
+        binding.imageView.scaleType = scaleType
+    }
+
+    fun setImage(
+        imagePath: File,
+        isCircularImage: Boolean = false,
+        needBorderWithCircularImage: Boolean = false
+    ) {
+        mode = Mode.DOWNLOAD
+        initiate()
+
+        val bitmap = BitmapFactory.decodeFile(imagePath.absolutePath)
+        var toSetHeight = bitmap.height
+        var toSetWidth = bitmap.width
+        val percentageChange: Float
+        if (
+            bitmap.height > binding.imageView.height || bitmap.width > binding.imageView.width
+        ) {
+            if (bitmap.width > bitmap.height) {
+                toSetWidth = binding.imageView.width
+                percentageChange =
+                    (bitmap.width.toFloat() - binding.imageView.width.toFloat()) / bitmap.width.toFloat()
+                toSetHeight = (bitmap.height - (bitmap.height * percentageChange)).toInt()
+            } else {
+                toSetHeight = binding.imageView.height
+                percentageChange =
+                    (bitmap.height.toFloat() - binding.imageView.height.toFloat()) / bitmap.height.toFloat()
+                toSetWidth = (bitmap.width - (bitmap.width * percentageChange)).toInt()
+            }
+        }
+        var scaledBitmap = Bitmap.createScaledBitmap(
+            bitmap,
+            toSetWidth,
+            toSetHeight,
+            true
+        )
+        if (isCircularImage && needBorderWithCircularImage) {
+            scaledBitmap = CircleTransformWithBorder().transform(scaledBitmap)
+        } else if (isCircularImage) {
+            scaledBitmap = CircleTransformation().transform(scaledBitmap)
+        }
+        binding.imageView.scaleType = scaleType
+        binding.imageView.setImageBitmap(
+            scaledBitmap
+        )
+        doneLoadingImage()
+
+        imageLoadedVia = ImageLoadedVia.BITMAP
+        imageBitmap = scaledBitmap
+    }
+
+    fun setImage(
+        imageUrl: String,
+        isCircularImage: Boolean = false,
+        needBorderWithCircularImage: Boolean = false,
+        needPlaceHolderImageForName: String = "",
+        appCompatImageView: AppCompatImageView? = null,
+        picassoCallback: Callback? = null
+    ) {
+        this.imageUrl = imageUrl
+        imageLoadedVia = ImageLoadedVia.URL
+
+        mode = Mode.DOWNLOAD
+        initiate()
+
+        binding.imageView.scaleType = scaleType
+
+        try {
+            val requestCreator: RequestCreator =
+                Picasso.get()
+                    .load(imageUrl)
+
+            if (scaleType == ImageView.ScaleType.CENTER_CROP) {
+                requestCreator
+                    .centerCrop()
+                    .resize(binding.imageView.measuredWidth, binding.imageView.measuredHeight)
+            }
+
+            if (isCircularImage && needBorderWithCircularImage) {
+                requestCreator.transform(
+                    CircleTransformWithBorder()
+                )
+            } else if (isCircularImage) {
+                requestCreator.transform(
+                    CircleTransformation()
+                )
+            }
+
+            if (needPlaceHolderImageForName != "") {
+                TextDrawableUtil.getPlaceHolder(
+                    needPlaceHolderImageForName,
+                    if (isCircularImage) TextDrawableUtil.Shape.ROUND else TextDrawableUtil.Shape.RECTANGLE
+                )?.let {
+                    requestCreator.placeholder(
+                        it
+                    )
+                }
+            }
+
+            requestCreator
+                .into(
+                    appCompatImageView ?: binding.imageView,
+                    picassoCallback ?: object : Callback {
+                        override fun onSuccess() {
+                            doneLoadingImage()
+                        }
+
+                        override fun onError(e: Exception?) {
+                        }
+                    }
+                )
+        } catch (exception: Exception) {
+            Handler().postDelayed(
+                {
+                    setImage(
+                        imageUrl,
+                        isCircularImage,
+                        needBorderWithCircularImage,
+                        needPlaceHolderImageForName
+                    )
+                },
+                200
+            )
+        }
+    }
+
+    fun setImage(imageDrawable: Drawable) {
+        this.imageDrawable = imageDrawable
+        imageLoadedVia = ImageLoadedVia.DRAWABLE
+        mode = Mode.DOWNLOAD
+        initiate()
+        binding.imageView.scaleType = scaleType
+        binding.imageView.setImageDrawable(imageDrawable)
+    }
+
+    fun putPlaceHolder(name: String, isCircularImage: Boolean = false) {
+        mode = Mode.DOWNLOAD
+        initiate()
+
+        binding.imageView.scaleType = scaleType
+        try {
+            val requestCreator: RequestCreator =
+                Picasso.get()
+                    .load("https://dummy.com/this_is_not_an_image.jpg")
+
+            if (scaleType == ImageView.ScaleType.CENTER_CROP) {
+                requestCreator
+                    .centerCrop()
+                    .resize(binding.imageView.measuredWidth, binding.imageView.measuredHeight)
+            }
+
+            TextDrawableUtil.getPlaceHolder(
+                name,
+                if (isCircularImage) TextDrawableUtil.Shape.ROUND else TextDrawableUtil.Shape.RECTANGLE
+            )?.let {
+                requestCreator.placeholder(
+                    it
+                )
+            }
+
+            requestCreator
+                .into(
+                    binding.imageView,
+                    object : Callback {
+                        override fun onSuccess() {
+                            doneLoadingImage()
+                        }
+
+                        override fun onError(e: Exception?) {
+                        }
+                    }
+                )
+        } catch (exception: Exception) {
+            Handler().postDelayed(
+                {
+                    putPlaceHolder(name, isCircularImage)
+                },
+                200
+            )
+        }
+    }
+
+    fun initiatingOrOnProgressUpload(identifier: String? = null, imageFile: File? = null): String {
+        imageFile?.let {
+            setImage(imageFile)
+        }
+        mode = Mode.UPLOAD
+        binding.progressUpload.visibility = View.VISIBLE
+        binding.progressUpload.indeterminateMode = true
+        identifier?.let {
+            this@ImageView.identifier = it
+        } ?: run {
+            this@ImageView.identifier = UUID.randomUUID().toString()
+        }
+        registerForUpdates()
+        return this.identifier
+    }
+
+    fun setUpdate(updatePercentage: Int) {
+        binding.progressUpload.indeterminateMode = false
+        binding.progressUpload.progressMax = 100F
+        binding.progressUpload.progress = updatePercentage.toFloat()
+
+        if (
+            updatePercentage == 100
+        ) {
+            binding.progressUpload.visibility = View.GONE
+        } else {
+            binding.progressUpload.visibility = View.VISIBLE
+        }
+    }
+
+    private fun doneLoadingImage() {
+        binding.progressDownload.visibility = View.GONE
+        binding.progressUpload.visibility = View.GONE
+    }
+
+    private fun registerForUpdates() {
+        context.registerReceiver(
+            object : BroadcastReceiver() {
+                override fun onReceive(context: Context?, intent: Intent?) {
+                    intent?.let {
+                        setUpdate(
+                            it.getIntExtra(
+                                UPLOAD_UPDATE,
+                                -1
+                            )
+                        )
+                    }
+                }
+            },
+            IntentFilter(
+                identifier
+            )
+        )
+    }
+
+    private fun changeZoomOfImage() {
+        Zoom(this)
+    }
+}
---
+++ widget/image_view/Zoom.kt
@@ -0,0 +1,104 @@
+package <YOUR PACKAGE HERE>;
+
+import android.app.Dialog
+import android.os.Handler
+import android.view.LayoutInflater
+import android.view.View
+import android.view.Window
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import com.sagar.android.pinchandzoom.databinding.ZoomBinding
+import com.squareup.picasso.Callback
+
+/*
+dependency req -
+    implementation 'com.otaliastudios:zoomlayout:1.7.0'
+*/
+class Zoom(imageView: ImageView) {
+
+    private var dialog: Dialog = Dialog(imageView.context)
+    private var binding: ZoomBinding = ZoomBinding.inflate(
+        LayoutInflater.from(imageView.context)
+    )
+
+    private val imageViewDoubleClickListener: View.OnClickListener = DoubleClickListener(
+        callback = object : DoubleClickListener.Callback {
+            override fun doubleClicked() {
+                close()
+            }
+        }
+    )
+
+    init {
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(binding.root)
+        dialog.window
+            ?.setLayout(
+                CoordinatorLayout.LayoutParams.MATCH_PARENT,
+                CoordinatorLayout.LayoutParams.MATCH_PARENT
+            )
+        dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+        dialog.setCancelable(true)
+
+        binding.imageView.scaleType = imageView.binding.imageView.scaleType
+
+        binding.zoomLayout.engine.apply {
+            setZoomEnabled(true)
+            setFlingEnabled(true)
+            setHorizontalPanEnabled(true)
+            setScrollEnabled(true)
+            setHorizontalPanEnabled(true)
+            setVerticalPanEnabled(true)
+            setOneFingerScrollEnabled(true)
+            setTwoFingersScrollEnabled(true)
+            setThreeFingersScrollEnabled(true)
+        }
+        binding.imageView.setOnClickListener(imageViewDoubleClickListener)
+
+        when (imageView.imageLoadedVia) {
+            ImageView.ImageLoadedVia.DRAWABLE -> {
+                binding.imageView.setImageDrawable(imageView.imageDrawable)
+            }
+            ImageView.ImageLoadedVia.BITMAP -> {
+                binding.imageView.setImageBitmap(imageView.imageBitmap)
+            }
+            ImageView.ImageLoadedVia.URL -> {
+                imageView.setImage(
+                    imageUrl = imageView.imageUrl,
+                    appCompatImageView = binding.imageView,
+                    picassoCallback = object : Callback {
+                        override fun onSuccess() {
+                            Handler().postDelayed(
+                                {
+                                    binding.zoomLayout.zoomTo(
+                                        2F,
+                                        true
+                                    )
+                                },
+                                500
+                            )
+                        }
+
+                        override fun onError(e: Exception?) {
+                            dialog.dismiss()
+                        }
+                    }
+                )
+            }
+        }
+
+        dialog.show()
+    }
+
+    private fun close() {
+        binding.zoomLayout.zoomTo(
+            1F,
+            true
+        )
+        Handler().postDelayed(
+            {
+                dialog.dismiss()
+            },
+            500
+        )
+    }
+}
